9c1a9dec896ec6312b6ba00ef4a426154aa7e148
Daniel Halan <danielhalan@gmail.com>
2012-12-09 18:00:30 +0700

    Updated version info

--- src/ServiceBusMQManager/Properties/AssemblyInfo.cs
+++ src/ServiceBusMQManager/Properties/AssemblyInfo.cs
@@ -22,11 +22,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceBus MQ Manager")]
-[assembly: AssemblyDescription("ServiceBus MQ Manager")]
+[assembly: AssemblyTitle("Service Bus MQ Manager")]
+[assembly: AssemblyDescription("Service Bus MQ Manager")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ITQ.COM")]
-[assembly: AssemblyProduct("ServiceBus MQ Manager")]
+[assembly: AssemblyProduct("Service Bus MQ Manager")]
 [assembly: AssemblyCopyright("Copyright ©2012 ITQ.COM, all rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -66,5 +66,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.100.0")]
-[assembly: AssemblyFileVersion("2.0.100.0")]
+[assembly: AssemblyVersion("2.0.120.0")]
+[assembly: AssemblyFileVersion("2.0.120.0")]
