1cd4b1990e429f8668e0112bcc5f588d42640dce
Daniel Halan <danielhalan@gmail.com>
2013-01-13 13:29:03 +0700

    Improved MainWindow UI

--- src/ServiceBusMQ/Model/QueueItem.cs
+++ src/ServiceBusMQ/Model/QueueItem.cs
@@ -15,17 +15,37 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace ServiceBusMQ.Model {
 
-  public enum QueueType { Command=0, Event, Message, Error }
+  public enum QueueType { Command = 0, Event, Message, Error }
 
   public class QueueItem {
 
     public string DisplayName { get; set; }
+
+    public double TextWidth {
+      get {
+
+        var formattedText = new System.Windows.Media.FormattedText(
+             DisplayName,
+             CultureInfo.CurrentCulture,
+             FlowDirection.LeftToRight,
+             new Typeface("Calibri"),
+             15.5,
+             Brushes.Black);
+
+        return formattedText.Width + 20;
+
+      }
+    }
+
 
     public string QueueDisplayName { get; set; }
     public string QueueName { get; set; }
@@ -36,9 +56,9 @@ namespace ServiceBusMQ.Model {
     public string[] MessageNames { get; set; }
 
     public DateTime ArrivedTime { get; set; }
-    public string ArrivedTimeString { 
-    
-      get { 
+    public string ArrivedTimeString {
+
+      get {
         if( ArrivedTime.Date == DateTime.Today.Date ) {
           return ArrivedTime.ToString("HH:mm:ss");
         } else return string.Format("{1}/{0} {2}", ArrivedTime.Month, ArrivedTime.Day, ArrivedTime.ToString("HH:mm:ss"));
