27bd99c7ee896711d4ca9dd9df4d13cb79fee5d0
Daniel Halan <danielhalan@gmail.com>
2013-01-22 12:13:00 +0700

    Added File Info.

--- src/ServiceBusMQManager/Controls/CommandTextEditor.xaml.cs
+++ src/ServiceBusMQManager/Controls/CommandTextEditor.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+#region File Information
+/********************************************************************
+  Project: ServiceBusMQManager
+  File:    CommandTextEditor.xaml.cs
+  Created: 2013-01-19
+
+  Author(s):
+    Daniel Halan
+
+ (C) Copyright 2013 Ingenious Technology with Quality Sweden AB
+     all rights reserved
+
+********************************************************************/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
