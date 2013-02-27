09a8a507e544049583a91ccc325b53960f25ae85
Daniel Halan <danielhalan@gmail.com>
2013-02-27 23:22:27 +0700

    Removed Microsoft.Expression ref.
    Solved Issue #4

--- src/ServiceBusMQManager/Controls/ClockControl.xaml.cs
+++ src/ServiceBusMQManager/Controls/ClockControl.xaml.cs
@@ -26,7 +26,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Expression.Shapes;
 using ServiceBusMQ;
 
 namespace ServiceBusMQManager.Controls {
