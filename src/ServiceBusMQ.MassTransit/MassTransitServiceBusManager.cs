52e2ee7a82b29a4be1f5c300d59d3d9408dbeedc
jjchiw <juan.chiw@cantera-tech.net>
2013-08-29 16:38:20 +0200

    Masstransit sin funcionar, llevandolo a la casa

---
+++ src/ServiceBusMQ.MassTransit/MassTransitServiceBusManager.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceBusMQ.Manager;
+using ServiceBusMQ.Model;
+using System.Xml.Linq;
+using System.IO;
+using System.Runtime.CompilerServices;
+using ServiceBusMQ.Manager;
+using System.Messaging;
+using NLog;
+using System.Threading;
+using MassTransit;
+using System.Xml.Serialization;
+
+namespace ServiceBusMQ.MassTransit
+{
+	public class MassTransitServiceBusManager : IServiceBusManager
+	{
+
+		public QueueFetchResult GetProcessedMessages(QueueType type, DateTime since, IEnumerable<QueueItem> currentItems)
+		{
+			return new QueueFetchResult
+			{
+				Count = 1,
+				Items = currentItems
+			};
+		}
+
+		public QueueFetchResult GetUnprocessedMessages(QueueType type, IEnumerable<QueueItem> currentItems)
+		{
+			return new QueueFetchResult
+			{
+				Count = 1,
+				Items = currentItems
+			};
+		}
+
+		Queue[] _monitorQueues;
+		string _serverName;
+		SbmqmMonitorState _monitorState;
+
+		public void Initialize(string serverName, Queue[] monitorQueues, SbmqmMonitorState monitorState)
+		{
+			_serverName = serverName;
+			_monitorState = monitorState;
+			_monitorQueues = monitorQueues;
+		}
+
+		public event EventHandler ItemsChanged;
+
+		public string LoadMessageContent(QueueItem itm)
+		{
+			return itm.Content;
+		}
+
+		public bool MessagesHasMilliSecondPrecision
+		{
+			get { return false; }
+		}
+
+		public Queue[] MonitorQueues
+		{
+			get { return _monitorQueues; }
+		}
+
+		public void MoveAllErrorMessagesToOriginQueue(string errorQueue)
+		{
+			
+		}
+
+		public void MoveErrorMessageToOriginQueue(QueueItem itm)
+		{
+			
+		}
+
+		public void PurgeAllMessages()
+		{
+			
+		}
+
+		public void PurgeErrorAllMessages()
+		{
+			
+		}
+
+		public void PurgeErrorMessages(string queueName)
+		{
+			
+		}
+
+		public void PurgeMessage(QueueItem itm)
+		{
+			
+		}
+
+		public void Terminate()
+		{
+			
+		}
+
+		public event EventHandler<Manager.WarningArgs> WarningOccured;
+
+		public string[] AvailableMessageContentTypes
+		{
+			get { return new string[] { "XML", "JSON" }; }
+		}
+
+		public string[] AvailableMessageQueueTypes
+		{
+			get { return new string[] { "XML", "JSON" }; }
+		}
+
+		public string ServiceBusName
+		{
+			get { return "MassTransit"; }
+		}
+
+		public event EventHandler<Manager.ErrorArgs> ErrorOccured;
+	}
+}
---
+++ src/ServiceBusMQ.MassTransit/MsmqMessageQueue.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceBusMQ.Model;
+using System.Messaging;
+using System.IO;
+
+namespace ServiceBusMQ.MassTransit
+{
+	public class MsmqMessageQueue
+		{
+
+			public Queue Queue { get; set; }
+
+			public MessageQueue Main { get; set; }
+			public MessageQueue Journal { get; set; }
+
+			public bool UseJournalQueue { get { return Main.UseJournalQueue; } }
+			public bool CanReadJournalQueue { get { return Main.UseJournalQueue && Journal.CanRead; } }
+
+			public MessageQueue _mainContent;
+			public MessageQueue _journalContent;
+
+
+			public MsmqMessageQueue(string serverName, Queue queue)
+			{
+				Queue = queue;
+
+				Main = Msmq.Create(serverName, queue.Name, QueueAccessMode.ReceiveAndAdmin);
+
+				_mainContent = Msmq.Create(serverName, queue.Name, QueueAccessMode.ReceiveAndAdmin);
+				_mainContent.MessageReadPropertyFilter.ClearAll();
+				_mainContent.MessageReadPropertyFilter.Body = true;
+
+
+				if (Main.UseJournalQueue)
+				{ // Error when trying to use FormatName, strange as it should work according to MSDN. Temp solution for now.
+					Journal = new MessageQueue(string.Format(@"{0}\Private$\{1};JOURNAL", serverName, queue.Name));
+
+					_journalContent = new MessageQueue(string.Format(@"{0}\Private$\{1};JOURNAL", serverName, queue.Name));
+					_journalContent.MessageReadPropertyFilter.ClearAll();
+					_journalContent.MessageReadPropertyFilter.Body = true;
+				}
+			}
+
+			public static implicit operator MessageQueue(MsmqMessageQueue q)
+			{
+				return q.Main;
+			}
+
+			public void LoadMessageContent(QueueItem itm)
+			{
+				Message msg = null;
+
+				if (!itm.Processed)
+				{
+					try
+					{
+						msg = _mainContent.PeekById(itm.Id);
+
+					}
+					catch
+					{
+
+						if (_journalContent != null)
+						{
+							try
+							{
+								msg = _journalContent.ReceiveById(itm.Id);
+
+							}
+							catch
+							{
+								itm.Content = "**MESSAGE HAS BEEN PROCESSED OR PURGED**";
+							}
+
+						}
+						else itm.Content = "**MESSAGE HAS BEEN PROCESSED OR PURGED AND JOURNALING IS TURNED OFF**";
+
+					}
+				}
+				else
+				{
+
+					if (_journalContent != null)
+					{
+
+						try
+						{
+							msg = _journalContent.ReceiveById(itm.Id);
+						}
+						catch
+						{
+							itm.Content = "**MESSAGE HAS BEEN PURGED FROM JOURNAL**";
+						}
+
+					}
+					else
+					{
+						itm.Content = "**MESSAGE HAS BEEN PROCESSED OR PURGED AND JOURNALING IS TURNED OFF**";
+					}
+
+				}
+
+				if (msg != null)
+					itm.Content = ReadMessageStream(msg.BodyStream);
+			}
+			private string ReadMessageStream(Stream s)
+			{
+				using (StreamReader r = new StreamReader(s, Encoding.Default))
+					return r.ReadToEnd().Replace("\0", "");
+			}
+
+
+			internal string GetDisplayName()
+			{
+				return "hola enfermera";
+				//return Main.GetDisplayName();
+			}
+
+			internal void Purge()
+			{
+				Main.Purge();
+			}
+
+
+
+		}
+}
