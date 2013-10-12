17b8c76171ed7b9d443a0d877ae24a28c3cecc75
Daniel Halan <danielhalan@gmail.com>
2013-10-12 20:53:05 +0700

    Added NServiceBus v4 Adapter

---
+++ src/ServiceBusMQ.NServiceBus4/Msmq.cs
@@ -0,0 +1,35 @@
+#region File Information
+/********************************************************************
+  Project: ServiceBusMQ.NServiceBus
+  File:    Msmq.cs
+  Created: 2013-02-14
+
+  Author(s):
+    Daniel Halan
+
+ (C) Copyright 2013 Ingenious Technology with Quality Sweden AB
+     all rights reserved
+
+********************************************************************/
+#endregion
+
+using System.Messaging;
+
+namespace ServiceBusMQ.NServiceBus {
+  public static class Msmq {
+    
+    public static MessageQueue Create(string serverName, string queueName, QueueAccessMode accessMode) {
+      if( !queueName.StartsWith("private$\\") )
+        queueName = "private$\\" + queueName;
+
+      queueName = string.Format("FormatName:DIRECT=OS:{0}\\{1}", !Tools.IsLocalHost(serverName) ? serverName : ".", queueName);
+
+      return new MessageQueue(queueName, false, true, accessMode);
+    }
+    
+    public static MessageQueue Create(string queueFormatName, QueueAccessMode accessMode) {
+      return new MessageQueue(queueFormatName, false, true, accessMode);
+    }
+  
+  }
+}
