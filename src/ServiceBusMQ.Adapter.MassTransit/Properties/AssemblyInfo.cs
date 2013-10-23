a8f22f1be6241a7b6ae0bf58d4a5fe51dfda3c8a
Daniel Halan <danielhalan@gmail.com>
2013-10-23 21:33:25 +0700

    Fixed issues with Asm Rescanning

--- src/ServiceBusMQ.Adapter.MassTransit/Properties/AssemblyInfo.cs
+++ src/ServiceBusMQ.Adapter.MassTransit/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ServiceBusMQ.Adapter.MassTransit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ServiceBusMQ.Adapter.MassTransit")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.10.34.0")]
+[assembly: AssemblyFileVersion("3.10.34.0")]
