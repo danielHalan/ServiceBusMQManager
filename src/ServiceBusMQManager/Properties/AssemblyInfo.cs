33ca7301b68c304f7a60933a946b3ff12add5991
Daniel Halan <danielhalan@gmail.com>
2014-03-27 11:18:08 +0700

    Upgraded NSB4 to 4.4.2

--- src/ServiceBusMQManager/Properties/AssemblyInfo.cs
+++ src/ServiceBusMQManager/Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.00.689.0")]
-[assembly: AssemblyFileVersion("4.00.689.0")]
+[assembly: AssemblyVersion("4.50.720.0")]
+[assembly: AssemblyFileVersion("4.50.720.0")]
